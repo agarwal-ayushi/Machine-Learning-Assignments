9958e9b843687e0a29b6c20bd7c8f22ad728120d
Ayushi Agarwal <ayushi.mnnit@gmail.com>
2020-02-09 17:26:43 +0530

    Complete

---
+++ Assignment1/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo "Running for Question 1"
+sleep 1
+python ques1/ques1_final.py
+
+sleep 3
+
+echo "Running for Question 2"
+sleep 1
+python ques2/ques2_final.py
+
+sleep 3
+
+echo "Running for Question 3"
+sleep 1
+python ques3/question3.py
+
+sleep 3
+
+echo "Running for Question 4"
+sleep 1
+python ques4/ques4-final.py
