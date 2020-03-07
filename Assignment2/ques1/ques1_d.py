4e010e7f9d14a11713b882795f2a91eecf0dbb57
Ayushi Agarwal <ayushi.mnnit@gmail.com>
2020-03-07 12:05:33 +0530

    A2b

--- Assignment2/ques1/ques1_d.py
+++ Assignment2/ques1/ques1_d.py
@@ -7,7 +7,7 @@
 
 
 #Importing Data from the CSV file
-get_ipython().run_line_magic('matplotlib', 'inline')
+#get_ipython().run_line_magic('matplotlib', 'inline')
 
 # All imports
 import string
@@ -308,7 +308,7 @@ def plot_confusion_matrix(conf_matrix):
     ax.set_ylim([0,2]) # Workaround to display values in the center, to avoid downgrade to matplotlib3.1.1
     ax.set_xlabel('Actual labels');
     ax.set_ylabel('Predicted labels'); 
-    ax.set_title('Confusion Matrix for NB using raw data'); 
+    ax.set_title('Confusion Matrix for NB'); 
     ax.xaxis.set_ticklabels(['y=0', 'y=1']); ax.yaxis.set_ticklabels(['y=0', 'y=1']);
     #plt.savefig('conf_matrix_Partc.png', dpi=1000, bbox_inches='tight')
 
@@ -357,6 +357,314 @@ def plot_roc(prob4):
 
 # In[ ]:
 
+plot_roc(prob4) # ROC of Processed data with UNIGRAMS only
 
-plot_roc(prob4)
 
+# # Feature 1: Remove the number of words with very less frequency
+
+print("----------------@@@@@@@@@@FEATURE - 1 Dictionary Refinement-----------------------")
+
+
+# In[ ]:
+
+
+# (1) Feature - We see that the number of words with very less frequency is very high. So we can actually 
+# Discard those words since they will not play much role in deciding the class of a tweet
+# This would be for words like numbers, or language specific words like muera, wrong spelling words, etc. 
+# We don't need to modify dict_0 and dict_4 since they will only be accessed for words found in the global
+# dictionary 
+
+cutoff_freq = 40 #40 is the optimum for 83% accuracy
+n_cutoff = len(dictionary)-sum(nw[0:cutoff_freq]) 
+print("Number of words with frequency higher than cutoff frequency({}) :".format(cutoff_freq),n_cutoff)
+
+dictionary_raw_new = {}
+for key in dictionary:
+    if dictionary[key] >=cutoff_freq:
+        dictionary_raw_new[key]=dictionary[key]
+
+theta_0_raw, theta_4_raw = train_nb_classifier(dictionary_raw_new, dict_0, dict_4, len_tweets, len_tweets_0, len_tweets_4, class_0, class_4)
+
+
+# In[62]:
+
+
+train_accuracy,actual_class_train,pred_class_train, prob0, prob4 = test_data(tweets_train_pd['lemma'], theta_0_raw, theta_4_raw, train_classes, phi_0, phi_4)
+print("Result (e) : The train accuracy of the model on processed data is with cutoff frequency = {}%".format(train_accuracy))
+
+
+# In[63]:
+
+
+test_accuracy, actual_class,pred_class, prob0, prob4 = test_data(tweets_test_pd['lemma'], theta_0_raw, theta_4_raw, test_actual_classes, phi_0, phi_4)
+print("Result (e) : The test accuracy of the model on processed data is with cutoff frequency is = {}%".format(test_accuracy))
+
+
+#Effect of cutoff-frequency on Train and Test Accuracy without bigrams
+cutoff = [0, 20, 35, 40, 50, 55, 60, 75, 100]
+train_acc = [79.18, 76.92, 76.71,76.78,76.59,76.55,76.51,76.42,76.30]
+test_acc = [80.50, 81.89, 82.45, 82.72, 82.45, 81.615, 81.615, 81.615, 81.615]
+plt.plot(cutoff, train_acc, marker='o', markersize=4, c='m', label='Train Accuracy')
+plt.plot(cutoff, test_acc, marker='*', markersize=4, c='b', label ='Test Accurcay')
+plt.axvline(x=40, c='k', linestyle='--', linewidth=0.9)
+plt.scatter(0,79.07, marker='^', s=30, c='c', label='Train Acc without digits')
+plt.scatter(0,79.97, marker='^', s=30, c='g', label='Test Acc without digits')
+plt.scatter(0,79.25, marker='^', s=30, c='k', label='Train Acc with digits')
+plt.scatter(0,80.50, marker='^', s=30, c='r', label='Test Acc with digits')
+()
+plt.xlabel("cutoff frequency")
+plt.ylabel("Accuracy")
+plt.legend(fontsize=8)
+plt.title("Affect of Cut-off Frequency and \non Test and Train Accuracy")
+plt.savefig("cutoff_accuracy_feature1.png",dpi=1000, bbox_inches='tight')
+#plt.show()
+
+print("\n-----------------------Creating the Confusion Matrix for this Model---------------------------\n")
+
+conf_matrix = create_confusion_matrix(actual_class, pred_class)
+print("The confusion Matrix of the model with cutoff is = \n", conf_matrix)
+
+
+# In[ ]:
+
+
+#Print confusion matrix (Code help from stackoverflow)
+print("\n-------------Plotting Confusion Matrix and ROC curve for cutoff---------------------------\n")
+plot_confusion_matrix(conf_matrix)
+
+plot_roc(prob4) # ROC of Processed data with UNIGRAMS only with CUTOFF
+
+# # FEATURE  - BI-GRAMS only consecutive words
+
+# In[ ]:
+
+
+print("----------------@@@@@@@@@@FEATURE - 2 BIGRAMS-----------------------")
+
+
+# In[ ]:
+
+
+import itertools 
+
+tweets_test =tweets_test_pd['lemma'].apply(lambda x: list(map(list, zip(x, x[1:]))))
+tweets_test['bigrams_str']=tweets_test.apply(lambda x: [' '.join(x[i]) for i in range(len(x))])
+
+tweets_train=tweets_train_pd['lemma'].apply(lambda x: list(map(list, zip(x, x[1:]))))
+tweets_train['bigrams_str']=tweets_train.apply(lambda x: [' '.join(x[i]) for i in range(len(x))])
+
+
+# In[ ]:
+
+
+print("-----------------Create Dictionary with bigrams---------------------------")
+
+dictionary_bigrams_1, dict_0_bigrams, dict_4_bigrams, len_bigrams, len_bigrams_0, len_bigrams_4 = create_dict(tweets_train['bigrams_str'], train_classes)
+
+
+# In[ ]:
+
+
+nw = [0 for i in range(max(dictionary_bigrams_1.values())+1)] 
+f = [i for i in range(max(dictionary_bigrams_1.values())+1)] 
+for key in dictionary_bigrams_1:
+    nw[dictionary_bigrams_1[key]]+=1
+plt.plot(f,nw)
+plt.axis([40, 80, 0, 2000])
+plt.xlabel("Frequency")
+plt.ylabel("No of words")
+#plt.show()
+
+
+# In[ ]:
+
+
+# (2) Using BIGRAMS - only consecutive words
+# We don't need to modify dict_0 and dict_4 since they will only be accessed for words found in the global
+# dictionary 
+
+cutoff_freq = 50
+n_cutoff = len(dictionary_bigrams_1)-sum(nw[0:cutoff_freq]) 
+print("Number of words with frequency higher than cutoff frequency({}) :".format(cutoff_freq),n_cutoff)
+
+dictionary_bigrams_new = {}
+for key in dictionary_bigrams_1:
+    if dictionary_bigrams_1[key] >=cutoff_freq:
+        dictionary_bigrams_new[key]=dictionary_bigrams_1[key]
+
+
+# In[ ]:
+
+
+#Add the bigrams dictionary to raw dictionary of unigrams
+dictionary_bigrams_new.update(dictionary_raw_new)
+dict_0_bigrams.update(dict_0)
+dict_4_bigrams.update(dict_4)
+
+#Effect of cutoff-frequency on Train and Test Accuracy - Raw Dictionary+Bigrams
+cutoff = [0, 10, 30, 40, 50]
+train_acc = [79.25, 80.94, 80.112, 79.97, 79.89]
+test_acc =  [80.50, 81.61, 81.61, 81.62, 81.05]
+plt.plot(cutoff, train_acc, marker='o', markersize=4, c='m', label='Train Accuracy')
+plt.plot(cutoff, test_acc, marker='*', markersize=4, c='b', label ='Test Accurcay')
+plt.axvline(x=10, c='k', linestyle='--', linewidth=0.9, label='Max Test Accuracy')
+plt.axvline(x=0, c='r', linestyle='--', linewidth=0.9, label='Overfitting')
+plt.scatter(0,91.468, marker='^', s=30, c='c', label='Train Acc with only bigrams')
+plt.scatter(0,76.02, marker='^', s=30, c='c', label='Test Acc with only bigrams')
+plt.scatter(0,75.36, marker='^', s=30, c='r', label='Train Acc with only bigrams (cutoff=10)')
+plt.scatter(0,77.15, marker='^', s=30, c='r', label='Test Acc with only bigrams(cutoff=10)')
+
+plt.xlabel("cutoff frequency")
+plt.ylabel("Accuracy")
+plt.legend(fontsize=8)
+plt.title("Affect of Cut-off Frequency on Test and \nTrain Accuracy with bigrams plus raw dictionary")
+plt.savefig("Accuracy_with_Bigrams_and_Raw+cutoff.png",dpi=1000, bbox_inches='tight')
+#plt.show()
+
+
+# In[ ]:
+
+
+print("\n------------------Learning NB on Unigrams+Bigrams-------------------------------\n")
+
+theta_0_bigrams, theta_4_bigrams = train_nb_classifier(dictionary_bigrams_new, dict_0_bigrams, dict_4_bigrams, len_tweets, len_tweets_0, len_tweets_4, class_0, class_4)
+
+
+# In[ ]:
+
+
+
+train_accuracy,actual_class_train,pred_class_train, prob0, prob4  = test_data(tweets_train_pd['lemma']+tweets_train['bigrams_str'], theta_0_bigrams, theta_4_bigrams, train_classes, phi_0, phi_4)
+print("Result (e) : The train accuracy of the model on bigrams+unigrams data is = {}%".format(train_accuracy))
+
+test_accuracy, actual_class,pred_class, prob0, prob4 = test_data((tweets_test_pd['lemma']+tweets_test['bigrams_str']), theta_0_bigrams, theta_4_bigrams, test_actual_classes, phi_0, phi_4)
+print("Result (e) : The test accuracy of the model on bigrams+unigrams data is = {}%".format(test_accuracy))
+
+print("\n-----------------------Creating the Confusion Matrix for this Model---------------------------\n")
+
+conf_matrix = create_confusion_matrix(actual_class, pred_class)
+print("The confusion Matrix of the model with Bigrams is = \n", conf_matrix)
+
+
+# In[ ]:
+
+
+#Print confusion matrix (Code help from stackoverflow)
+print("\n-------------Plotting Confusion Matrix and ROC curve for BI+UNIGRAMS MODEL---------------------------\n")
+plot_confusion_matrix(conf_matrix)
+
+plot_roc(prob4) # ROC of Processed data with UNIGRAMS+BIGRAMS 
+
+# # FEATURE  - TRI-GRAMS only consecutive words
+
+# In[ ]:
+
+
+print("----------------@@@@@@@@@@FEATURE - 3 TRIGRAMS-----------------------")
+
+
+# In[ ]:
+
+
+tweets_test =tweets_test_pd['lemma'].apply(lambda x: list(map(list, zip(x, x[1:],x[2:]))))
+tweets_test['trigrams_str']=tweets_test.apply(lambda x: [' '.join(x[i]) for i in range(len(x))])
+
+
+# In[ ]:
+
+
+tweets_train=tweets_train_pd['lemma'].apply(lambda x: list(map(list, zip(x, x[1:], x[2:]))))
+tweets_train['trigrams_str']=tweets_train.apply(lambda x: [' '.join(x[i]) for i in range(len(x))])
+
+
+# In[ ]:
+
+
+print("\n-----------------Create Dictionary with trigrams---------------------------")
+
+
+dictionary_trigrams_1, dict_0_trigrams, dict_4_trigrams, len_trigrams, len_trigrams_0, len_trigrams_4 = create_dict(tweets_train['trigrams_str'], train_classes)
+
+
+# In[ ]:
+
+
+# (2) Using TRIGRAMS - only consecutive words
+# We don't need to modify dict_0 and dict_4 since they will only be accessed for words found in the global
+# dictionary 
+
+cutoff_freq = 10
+n_cutoff = len(dictionary_trigrams_1)-sum(nw[0:cutoff_freq]) 
+print("Number of words with frequency higher than cutoff frequency({}) :".format(cutoff_freq),n_cutoff)
+
+dictionary_trigrams_new = {}
+for key in dictionary_trigrams_1:
+    if dictionary_trigrams_1[key] >=cutoff_freq:
+        dictionary_trigrams_new[key]=dictionary_trigrams_1[key]
+
+
+# In[ ]:
+
+
+dictionary_trigrams_new.update(dictionary_raw_new)
+dict_0_trigrams.update(dict_0)
+dict_4_trigrams.update(dict_4)
+
+
+# In[ ]:
+
+
+print("\n------------------Learning NB on Unigrams+Trigrams-------------------------------\n")
+
+theta_0_trigrams, theta_4_trigrams = train_nb_classifier(dictionary_trigrams_new, dict_0_trigrams, dict_4_trigrams, len_tweets, len_tweets_0, len_tweets_4, class_0, class_4)
+
+
+# In[ ]:
+
+
+train_accuracy,actual_class_train,pred_class_train, prob0, prob4 = test_data(tweets_train_pd['lemma']+tweets_train['trigrams_str'], theta_0_trigrams, theta_4_trigrams, train_classes, phi_0, phi_4)
+print("Result (e) : The train accuracy of the model on Unigrams+Trigrams data is = {}%".format(train_accuracy))
+
+
+# In[ ]:
+
+
+test_accuracy, actual_class,pred_class, prob0, prob4 = test_data((tweets_test_pd['lemma']+tweets_test['trigrams_str']), theta_0_trigrams, theta_4_trigrams, test_actual_classes, phi_0, phi_4)
+print("Result (e) : The test accuracy of the model on Unigrams+Trigrams data is = {}%".format(test_accuracy))
+
+
+# In[ ]:
+
+
+#Effect of cutoff-frequency on Train and Test Accuracy - Raw Dictionary(with cutoff=40)+Trigrams(with cutoff)
+cutoff = [0, 10, 15, 20, 30, 40, 60, 100]
+train_acc = [94.80, 77.50, 77.31, 77.20, 77.09, 77.03, 76.97, 76.92]
+test_acc =  [82.17, 83.28, 82.72, 83.00, 82.73, 82.45, 82.45, 82.72]
+plt.plot(cutoff, train_acc, marker='o', markersize=4, c='m', label='Train Accuracy')
+plt.plot(cutoff, test_acc, marker='*', markersize=4, c='b', label ='Test Accurcay')
+plt.axvline(x=10, c='k', linestyle='--', linewidth=0.9, label='Max Test Accuracy')
+plt.axvline(x=0, c='r', linestyle='--', linewidth=0.9, label='Overfitting')
+plt.scatter(0,76.78, marker='^', s=30, c='k', label='Train Acc with raw data')
+plt.scatter(0,82.72, marker='^', s=30, c='r', label='Test Acc with raw data')
+
+plt.xlabel("cutoff frequency")
+plt.ylabel("Accuracy")
+plt.legend(fontsize=8)
+plt.title("Affect of Cut-off Frequency on Test and \nTrain Accuracy with trigrams \nplus raw dictionary(cutoff=40)")
+plt.savefig("Accuracy_with_trigrams_and_Raw_with_cutoff.png",dpi=1000, bbox_inches='tight')
+#plt.show()
+
+print("\n-----------------------Creating the Confusion Matrix for this Model---------------------------\n")
+
+conf_matrix = create_confusion_matrix(actual_class, pred_class)
+print("The confusion Matrix of the model with TRIgrams is = \n", conf_matrix)
+
+
+# In[ ]:
+
+
+#Print confusion matrix (Code help from stackoverflow)
+print("\n-------------Plotting Confusion Matrix and ROC curve for TRI+UNIGRAMS MODEL---------------------------\n")
+plot_confusion_matrix(conf_matrix)
+
+plot_roc(prob4) # ROC of Processed data with UNIGRAMS+BIGRAMS 
